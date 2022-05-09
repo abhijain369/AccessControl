23b49ca9e33aaf6b46737864e8d88b51e6a68a82
coach4dev <info@coach4dev.com>
2022-05-09 10:09:20 -0400

    Add Crypto Example

---
+++ Cryptography/CommonUsers.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cryptography
+{
+    public static class CommonUsers
+    {
+        public static List<User> Get() 
+        {
+            var users = new List<User>();
+            var user1 = new User()
+            {
+                Username = "User1",
+                Password = "123456"
+            };
+            users.Add(user1);
+
+            var user2 = new User()
+            {
+                Username = "User2",
+                Password = "password"
+            };
+            users.Add(user2);
+
+            var user4 = new User()
+            {
+                Username = "User4",
+                Password = "111111"
+            };
+            users.Add(user4);
+
+            var user5 = new User()
+            {
+                Username = "User5",
+                Password = "12345678"
+            };
+            users.Add(user5);
+
+            var user6 = new User()
+            {
+                Username = "User6",
+                Password = "qwerty"
+            };
+            users.Add(user6);
+
+            var user7 = new User()
+            {
+                Username = "User7",
+                Password = "abc123"
+            };
+            users.Add(user7);
+            return users;
+        }
+    }
+}
---
+++ Cryptography/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Cryptography
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var users = CommonUsers.Get();
+            Console.WriteLine("Common Password and their MD5 hashes");
+            foreach (var user in users) 
+            {
+                Console.WriteLine(user.Username + "     "  + user.Password + "     "  + GetMd5Hash(user.Password)); 
+            }
+            Console.ReadLine();
+            /*Check https://hashtoolkit.com/common-passwords/ for MD5 hashes */
+            Console.WriteLine("Common Password and their MD5 hashes with salt");
+            
+            foreach (var user in users)
+            {
+                var hashWithSalt = GetMd5HashWithSalt(user.Password);
+                Console.WriteLine(user.Username + "     " + user.Password + "     " + hashWithSalt.Item1 + "     " + hashWithSalt.Item2);
+            }
+            Console.ReadLine();
+        }
+
+
+
+
+        private static string GetMd5Hash(string input)
+        {
+            var hasher = MD5.Create();
+            var data = hasher.ComputeHash(Encoding.Default.GetBytes(input));
+            var builder = new StringBuilder();
+            for (var i = 0; i < data.Length; i++)
+            {
+                builder.Append(data[i].ToString("x2"));
+            }
+            return builder.ToString();
+        }
+
+        private static Tuple<string,string> GetMd5HashWithSalt(string input)
+        {
+            var rng = new RNGCryptoServiceProvider();
+
+            var buff = new byte[8];
+
+            rng.GetBytes(buff);
+
+            var salt = Convert.ToBase64String(buff);
+
+            var hasher = MD5.Create();
+
+            var data = hasher.ComputeHash(Encoding.Default.GetBytes(input + salt));
+
+            var builder = new StringBuilder();
+
+            for (var i = 0; i < data.Length; i++)
+
+            {
+
+                builder.Append(data[i].ToString("x2"));
+
+            }
+
+
+            return new Tuple<string, string>(builder.ToString(), salt);
+        }
+    }
+}
